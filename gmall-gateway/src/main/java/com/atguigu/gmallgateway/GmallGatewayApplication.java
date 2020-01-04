4580fe3f7cd4162362394edd9e7c36579df87a06
焦泽鹏 <jzp.workspace@foxmail.com>
2020-01-04 11:41:09 +0800

    实现图片上传

--- gmall-gateway/src/main/java/com/atguigu/gmallgateway/GmallGatewayApplication.java
+++ gmall-gateway/src/main/java/com/atguigu/gmallgateway/GmallGatewayApplication.java
@@ -1,19 +1,16 @@
-package com.atguigu.gmall.oms;
+package com.atguigu.gmallgateway;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableSwagger2
-@MapperScan("com.atguigu.gmall.oms.dao")
-@RefreshScope
-public class GmallOmsApplication {
+public class GmallGatewayApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(GmallOmsApplication.class, args);
+        SpringApplication.run(GmallGatewayApplication.class, args);
     }
+
 }
